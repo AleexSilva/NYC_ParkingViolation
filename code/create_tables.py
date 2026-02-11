2c81defe78ebd846a6d198a2ee3f2a2c71b0f2e5
AleexSilva <silva.aleixs94@gmail.com>
2026-02-11 04:07:13 +0000

    DBT Init

---
+++ code/create_tables.py
@@ -0,0 +1,38 @@
+import duckdb
+import pandas as pd
+
+
+
+# Create Database
+
+sql_query = '''
+    SHOW TABLES
+'''
+
+with duckdb.connect('../data/nyc_parking_violation.db') as con:
+    display(con.sql(sql_query).df())
+    
+
+# Create 2 tables and import data
+
+sql_query_import_1= '''
+    CREATE OR REPLACE TABLE parking_violation_codes AS
+    SELECT *
+    FROM read_csv_auto(
+        '../data/dof_parking_violation_codes.csv',
+    normalize_names=True    
+    )    
+'''
+
+sql_query_import_2= '''
+    CREATE OR REPLACE TABLE parking_violation_2023 AS
+    SELECT *
+    FROM read_csv_auto(
+        '../data/parking_violations_issued_fiscal_year_2023_sample.csv',
+    normalize_names=True
+    )
+'''
+
+with duckdb.connect('../data/nyc_parking_violation.db') as con:
+    con.sql(sql_query_import_1)
+    con.sql(sql_query_import_2)
