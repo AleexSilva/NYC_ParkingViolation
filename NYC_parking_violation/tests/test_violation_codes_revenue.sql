ca24927c65b56af108de7489d738e203cdb8f59e
AleexSilva <silva.alexis94@gmail.com>
2026-02-13 02:29:23 +0000

    singular test

---
+++ NYC_parking_violation/tests/test_violation_codes_revenue.sql
@@ -0,0 +1,12 @@
+-- warning
+
+{{ config (severity = 'warn')}}
+SELECT
+    violation_code,
+    SUM(fee_usd) as total_revenue_usd
+FROM
+    {{ref('silver_parking_violation_codes')}}
+GROUP BY
+    violation_code
+HAVING
+    NOT(total_revenue_usd >=1)
